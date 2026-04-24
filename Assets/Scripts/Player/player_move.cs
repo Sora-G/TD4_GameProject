64f81ab7b43325f157ffd0ec4fe3a69d1050207f
sora_suzuki <k022g0047@g.neec.ac.jp>
2026-04-24 14:54:59 +0900

    プレイヤーの移動処理を追加

---
+++ Assets/Scripts/Player/player_move.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class player_move : MonoBehaviour
+{
+    //変数宣言
+    public float moveSpeed;//移動速度
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        moveSpeed = 2.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float move = Input.GetAxis("Vertical");
+        float turn = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.forward * move * moveSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.up * turn * 100f * Time.deltaTime);
+    }
+}
